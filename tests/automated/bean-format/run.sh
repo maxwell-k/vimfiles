230f6ca2cfa8e2ba58cf403ef35846ca4662c50a
Keith Maxwell <keith.maxwell@gmail.com>
2021-12-26 14:22:58 +0000

    Separate bean-format tests

--- tests/automated/bean-format/run.sh
+++ tests/automated/bean-format/run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-# tests/automated/vader/run.sh
+# tests/automated/bean-format/run.sh
 # Copyright 2021 Keith Maxwell
 # SPDX-License-Identifier: MPL-2.0
 # For debugging use `:Vader` instead of `:Vader!`
-vim "+packadd vader | Vader! run.vader"
+vim "+packadd vader | Vader! bean-format.vader"
