dea367515b8e47b0440e0b1d1d5ba69ff537a936
Keith Maxwell <keith.maxwell@gmail.com>
2021-12-26 15:11:30 +0000

    Add tests to js-yaml linter

---
+++ tests/automated/js-yaml/run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# tests/automated/js-yaml/run.sh
+# Copyright 2021 Keith Maxwell
+# SPDX-License-Identifier: MPL-2.0
+# For debugging use `:Vader` instead of `:Vader!`
+vim "+packadd vader" "+Vader! js-yaml.vader"
