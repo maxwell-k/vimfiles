ecbbf8984d6bc70bdc24695f74d8de7d3c2038c7
Keith Maxwell <keith.maxwell@gmail.com>
2022-02-06 23:47:12 +0000

    Switch to running playbooks against all
    So that they can be run where Ansible is not installed

--- tests/inside-alpine-edge.sh
+++ tests/inside-alpine-edge.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 apk add vim npm ansible git beancount \
-&& ansible-playbook -i, site.yaml \
+&& mkdir --parents /etc/ansible \
+&& {
+  printf '[all]\n' \
+  && printf 'localhost' \
+  && printf ' ansible_connection=local' \
+  && printf ' ansible_python_interpreter=python3' \
+  && printf '\n' ; } >>/etc/ansible/hosts \
+&& ansible-playbook site.yaml \
 && tests/run.sh
