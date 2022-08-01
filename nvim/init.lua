0b8e3bebd65b968e6916fcf229c0cd7a18e5dfc2
Keith Maxwell <keith.maxwell@gmail.com>
2022-08-01 23:03:28 +0100

    Format lua on save with Neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,21 @@
 vim.api.nvim_command("set packpath +=~/.config/nvim")
 require("lspconfig").pyright.setup({})
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("null-ls").setup({
+	sources = {
+		require("null-ls").builtins.formatting.stylua,
+	},
+	-- from https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save#sync-formatting
+	on_attach = function(client, bufnr)
+		if client.supports_method("textDocument/formatting") then
+			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+			vim.api.nvim_create_autocmd("BufWritePre", {
+				group = augroup,
+				buffer = bufnr,
+				callback = function()
+					vim.lsp.buf.format({ bufnr = bufnr })
+				end,
+			})
+		end
+	end,
+})
