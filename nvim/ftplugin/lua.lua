522df1e1afb90d9ea396634a24bd82c5571fae8b
Keith Maxwell <keith.maxwell@gmail.com>
2022-08-07 18:53:22 +0100

    Improve neovim configuration

---
+++ nvim/ftplugin/lua.lua
@@ -0,0 +1,12 @@
+vim.bo.shiftwidth = 4
+vim.bo.tabstop = 4 -- to match https://github.com/JohnnyMorganz/StyLua#options
+
+require("lspconfig").sumneko_lua.setup({
+	settings = { -- To avoid errors when editing neovim configuration files
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			diagnostics = { globals = { "vim", "utf8" } },
+			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+		},
+	},
+})
--- nvim/init.lua
+++ nvim/init.lua
@@ -1,19 +1,26 @@
+-- Basic options
 vim.o.hidden = false
-vim.api.nvim_command("set packpath +=~/.config/nvim")
-require("lspconfig").pyright.setup({})
+vim.o.packpath = "~/.config/nvim"
+vim.o.mouse = ""
+
+-- Packages
 require("nvim_comment").setup()
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+require("nvim-surround").setup()
+
+-- Mappings
+vim.api.nvim_set_keymap("n", "<Leader>w", "<C-w>", { noremap = true })
+
+-- Language server settings
+local venv = { only_local = ".venv/bin" }
+local sources = {
+	require("null-ls").builtins.formatting.black.with(venv),
+	require("null-ls").builtins.formatting.reorder_python_imports,
+	require("null-ls").builtins.formatting.stylua,
+	require("null-ls").builtins.diagnostics.flake8.with(venv),
+}
+local augroup = vim.api.nvim_create_augroup("Init", {})
 require("null-ls").setup({
-	sources = {
-		require("null-ls").builtins.formatting.reorder_python_imports,
-		require("null-ls").builtins.formatting.stylua,
-		require("null-ls").builtins.formatting.black.with({
-			only_local = ".venv/bin",
-		}),
-		require("null-ls").builtins.diagnostics.flake8.with({
-			only_local = ".venv/bin",
-		}),
-	},
+	sources = sources,
 	-- from https://github.com/jose-elias-alvarez/null-ls.nvim/wiki/Formatting-on-save#sync-formatting
 	on_attach = function(client, bufnr)
 		if client.supports_method("textDocument/formatting") then
@@ -26,5 +33,11 @@ require("null-ls").setup({
 				end,
 			})
 		end
+	end,
+})
+vim.api.nvim_create_autocmd("DiagnosticChanged", {
+	group = augroup,
+	callback = function()
+		vim.diagnostic.setloclist({ open = false })
 	end,
 })
