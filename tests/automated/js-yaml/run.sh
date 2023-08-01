f24fdf6d1067adbe96e05feeb19f7120cfc10c45
Keith Maxwell <keith.maxwell@gmail.com>
2023-08-01 23:19:38 +0100

    Remove js-yaml linter
    yamllint offers the same functionality and is supported by ALE

--- tests/automated/js-yaml/run.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# tests/automated/js-yaml/run.sh
-# Copyright 2021 Keith Maxwell
-# SPDX-License-Identifier: MPL-2.0
-# For debugging use `:Vader` instead of `:Vader!`
-vim "+packadd vader" "+Vader! unit.vader" \
-&& vim "+packadd vader" "+Vader! integration.vader"
