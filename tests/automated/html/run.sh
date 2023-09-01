b34a6646f86889366b452c8ccee90da6d31e85ed
Keith Maxwell <keith.maxwell@gmail.com>
2023-09-02 00:55:26 +0100

    Rename vader files to run.vader

--- tests/automated/html/run.sh
+++ tests/automated/html/run.sh
@@ -3,4 +3,4 @@
 # Copyright 2021 Keith Maxwell
 # SPDX-License-Identifier: MPL-2.0
 # For debugging use `:Vader` instead of `:Vader!`
-vim "+packadd vader | Vader! html.vader"
+vim "+packadd vader | Vader! run.vader"
