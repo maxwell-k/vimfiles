5e9fa4e7c2d176bf1892436828ad500877ed82bb
Keith Maxwell <keith.maxwell@gmail.com>
2025-01-16 23:28:17 +0000

    Fix a path in a test file

--- tests/automated/python/run.sh
+++ tests/automated/python/run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# tests/automated/vader/run.sh
+# tests/automated/python/run.sh
 # Copyright 2021 Keith Maxwell
 # SPDX-License-Identifier: MPL-2.0
 # For debugging use `:Vader` instead of `:Vader!`
