878e9bc33ebfc00e4f3a37e55de04754b1e08a38
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-04 13:03:12 +0100

    Reorganise spellcheck tests

--- tests/automated/spellcheck/run.sh
+++ tests/automated/spellcheck/run.sh
@@ -3,6 +3,6 @@
 # Copyright 2024 Keith Maxwell
 # SPDX-License-Identifier: MPL-2.0
 # For debugging use `:Vader` instead of `:Vader!`
-vim "+packadd vader | Vader! run.vader" \
+./spellcheck.vader \
 && echo "Text with no spelling mistakes." \
 | vim '+call spellcheck#Exit()' -
