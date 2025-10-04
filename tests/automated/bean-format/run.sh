0bbcd82cb31a2564b210ab742397af97fb22f714
Keith Maxwell <keith.maxwell@gmail.com>
2025-10-04 12:59:09 +0100

    Merge tests/automated/bean-format into tests/automated/simple-vader

--- tests/automated/bean-format/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# tests/automated/bean-format/run.sh
-# Copyright 2021 Keith Maxwell
-# SPDX-License-Identifier: MPL-2.0
-# For debugging use `:Vader` instead of `:Vader!`
-vim "+packadd vader | Vader! run.vader"
